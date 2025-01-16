a9260769277a4f034f412be7480a73d4da207765
Raul Ferrando <raulferrando@protonmail.com>
2025-01-16 17:40:42 +0100

    add jb

--- lua/raulf/plugins/colorscheme.lua
+++ lua/raulf/plugins/colorscheme.lua
@@ -3,8 +3,18 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         opts = {},
+        --config = function()
+            --vim.cmd.colorscheme('tokyonight-storm')
+       -- end
+    },
+    {
+        "nickkadutskyi/jb.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
         config = function()
-            vim.cmd.colorscheme('tokyonight-storm')
-        end
+            -- require("jb").setup({transparent = true})
+            vim.cmd("colorscheme jb")
+        end,
     }
 }
